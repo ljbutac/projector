b499b6ec9175e71402a1b590edcb327db589fec7
Love Joy <lovejoy120499@gmail.com>
2021-10-20 12:01:36 +0800

    new changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'username',
+        'lastname',
+        'firstname',
         'email',
         'password',
     ];
